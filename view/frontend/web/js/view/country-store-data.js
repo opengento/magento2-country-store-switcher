b5e74fdc87518aed150c0f52cc8ba20559e51d7e
Thomas Klein <thomasklein876@gmail.com>
2020-08-06 10:06:19 +0200

    refactor condition

--- view/frontend/web/js/view/country-store-data.js
+++ view/frontend/web/js/view/country-store-data.js
@@ -11,19 +11,8 @@ define([
 
     return function (CountryStoreData) {
         return CountryStoreData.extend({
-            initialize: function () {
-                this._super();
-
-                if (this._hasScopeChanged()) {
-                    customerData.set('country_store_data', {'reload': true});
-                    customerData.reload(['country_store_data']);
-                }
-            },
-
-            _hasScopeChanged: function () {
-                return BASE_URL !== this.countryStoreData().base_url &&
-                    !this.countryStoreData().reload &&
-                    !_.contains(customerData.getExpiredSectionNames(), 'country_store_data');
+            isInvalidated: function () {
+                return this._super() || BASE_URL !== this.countryStoreData().base_url;
             }
         });
     };
